2d7e7e6f8538375737fa2fcb45817fdacaa89c93
Daniel King <danking@ccs.neu.edu>
2013-05-20 21:28:05 -0400

    Initial commit

---
+++ forward-analysis.rkt
@@ -0,0 +1,91 @@
+#lang racket
+
+(require "../cfa2/cfa2.rkt"
+         "../cfa2/lattice.rkt"
+         "../semantics/abstract.rkt"
+         "../semantics/abstract-utilities.rkt"
+         "../semantics/flow.rkt"
+         (only-in "../pda-to-pda-risc/risc-enhanced/data.rkt"
+                  pda-risc-enh-initial-term
+                  pop-assign?
+                  push?))
+
+(provide forward-analysis)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Forward Analysis
+
+;; forward-analysis : [Lattice FV]
+;;                    [AbstractState -> FV]
+;;                    PDA-RISC-ENH
+;;                    ->
+;;                    [FlowAnalysis [Lattice FlowState]
+;;                                  [FlowState -> FV]]
+(define (forward-analysis flow-value-lattice
+                          fv-next
+                          pda-risc-enh
+                          #:debug [debug 0])
+  ;; push-fstate? : FlowState -> Boolean
+  (define push-fstate? (lift-insn/flow push?))
+
+  ;; pop-fstate? : FlowState -> Boolean
+  (define pop-fstate? (lift-insn/flow pop-assign?))
+
+  ;; pop-succ-states : AState AState -> [SetOf AState]
+  (define (pop-succ-states push pop)
+    (abstract-step/new-stack pop (abstract-state-st push)))
+
+  ;; flow-state-join : FlowState FlowState -> FlowState
+  (define flow-state-join
+    (maplift-astate*fv/flow astate-join
+                            (lattice-join flow-value-lattice)))
+
+  ;; flow-state-gte : FlowState FlowState -> Boolean
+  (define flow-state-gte
+    (foldlift-astate*fv/flow astate-gte
+                             (lattice-gte flow-value-lattice)
+                             (lambda (x y)
+                               (and x y))))
+
+  (define flow-state-lattice
+    (lattice flow-state-join
+             flow-state-gte
+             (lattice-bottom flow-value-lattice)
+             (lattice-top flow-value-lattice)))
+
+  ;; flow-state-similar? : FlowState FlowState -> Boolean
+  (define flow-state-similar?
+    (match-lambda* [(list (flow-state s1 _)
+                          (flow-state s2 _))
+                    (astate-similar? s1 s2)]))
+  ;; flow-state-hash-code : FlowState -> Number
+  (define flow-state-hash-code
+    (match-lambda [(flow-state as _) (astate-hash-code as)]))
+
+  ;; next-flow : FlowState -> FlowValue
+  (define (next-flow fstate)
+    (match-define (flow-state astate flow) fstate)
+
+    (fv-next astate flow))
+
+  ;; succ-states/flow : FlowState -> [SetOf FlowState]
+  (define (succ-states/flow fstate)
+    (match-define (flow-state astate fv) fstate)
+
+    (for/seteq ([astate~ (in-set (abstract-step astate))])
+      (flow-state astate~ (next-flow fstate))))
+
+  ;; pop-succ-states/flow : FlowState FlowState -> [SetOf FlowState]
+  (define (pop-succ-states/flow push-fstate pop-fstate)
+    (match-define (flow-state push-astate push-fv) push-fstate)
+    (match-define (flow-state pop-astate _) pop-fstate)
+
+    (for/seteq ([astate~ (in-set (pop-succ-states push-astate pop-astate))])
+      (flow-state astate~ (max push-fv (next-flow pop-fstate)))))
+
+  (FlowAnalysis (initial-flow-state (pda-risc-enh-initial-term pda-risc-enh)
+                                    (lattice-bottom flow-value-lattice))
+                push-fstate? pop-fstate? flow-state-equal?
+                flow-state-lattice
+                flow-state-similar? flow-state-hash-code
+                succ-states/flow pop-succ-states/flow))
---
+++ min-headroom.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require "forward-analysis.rkt"
+         "../cfa2/lattice.rkt"
+         "../semantics/abstract.rkt"
+         "../semantics/abstract-utilities.rkt"
+         (only-in "../pda-to-pda-risc/risc-enhanced/data.rkt"
+                  stack-ensure-hdrm
+                  pda-term-insn))
+
+(provide min-headroom-analysis
+         min-headroom-lattice
+         min-headroom-flow-function)
+
+(define (min-headroom-flow-function astate flow-value)
+  (cond [(push-astate? astate) (max (sub1 flow-value) 0)]
+        [(pop-astate? astate) (add1 flow-value)]
+        [(stack-ensure-astate? astate)
+         (+ flow-value
+            (stack-ensure-hdrm
+             (pda-term-insn
+              (abstract-state-node astate))))]
+        [else flow-value]))
+
+(define min-headroom-lattice
+  (lattice min <= 0 +inf.0))
+
+(define (min-headroom-analysis pda-risc)
+  (forward-analysis min-headroom-lattice
+                    min-headroom-flow-function
+                    pda-risc))
